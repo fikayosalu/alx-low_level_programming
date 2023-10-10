514ed40bc65c204e11db0f12fe635e8e2c866dea
fikayo salu <fikayosalu@gmail.com>
2023-10-10 16:55:37 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add- Function to add two integers
+ * @a:Parameter
+ * @b:Parameter
+ * Return: the result of the function
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
