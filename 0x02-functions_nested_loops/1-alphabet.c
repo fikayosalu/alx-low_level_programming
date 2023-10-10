65773e023ddc8679159305a1e4f0a7a022b24593
fikayo salu <fikayosalu@gmail.com>
2023-10-10 06:34:44 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet- Function that prints alphabet in lower case
+ * Return: No return value
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
