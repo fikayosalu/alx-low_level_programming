85655336302db8a8163f92b6ad8daea4139ffb2f
fikayo salu <fikayosalu@gmail.com>
2023-10-10 17:50:36 -0700

    Betty fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d", i);
-			if (i !=98)
+			if (i != 98)
 				printf(", ");
 		}
 		printf("\n");
@@ -25,8 +25,8 @@ void print_to_98(int n)
 		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d", i);
-			if (i !=98)
-                                printf(", ");
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -35,8 +35,8 @@ void print_to_98(int n)
 		for (i = n ; i >= 98 ; i--)
 		{
 			printf("%d", i);
-			if (i !=98)
-                                printf(", ");
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
