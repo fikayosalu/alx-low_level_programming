8676d3dfd4995e9f08f891b75cfaa8c29b1a4a15
fikayo salu <fikayosalu@gmail.com>
2023-10-12 14:05:12 -0700

    Numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers-Function that prints numbers from 0-9
+ * Return: no return value
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
