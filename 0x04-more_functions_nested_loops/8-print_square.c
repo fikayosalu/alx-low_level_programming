983741a2ae1ceb2c1ff2a40f32ac942933c22dae
fikayo salu <fikayosalu@gmail.com>
2023-10-12 17:56:34 -0700

    Square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square- Function that prints square
+ * @size:Parameter
+ * Return:no return value
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1 ; i <= size ; i++)
+		{
+			for (j = 1 ; j <= size ; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
