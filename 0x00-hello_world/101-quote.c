f718b920f6f0e778537ac4730c08bd37362eb226
fikayo salu <fikayosalu@gmail.com>
2023-10-20 13:55:37 -0700

    leet

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Where all C programs are written
+ * Return: 1 when successful
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
