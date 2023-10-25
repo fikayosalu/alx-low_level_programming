414caaebb25ae5b351eec5c32c15bb2b829ebb14
fikayo salu <fikayosalu@gmail.com>
2023-10-25 13:59:41 -0700

    Print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - Function that prints a sting and a new line
+ * @s: Parameter of the function
+ *
+ * Return: There is no return value
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
