e123bd54076cd95f34b331d89d63c91ddb5c17a9
fikayosalu <salufikayo@gmail.com>
2024-08-09 01:23:17 +0100

    task 5 flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,9 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int a, countbit = 0;
-	unsigned lont int current;
+
+	unsigned long int current;
+
 	unsigned long int exclusive = n ^ m;
 
 	for (a = 63; a >= 0; a--)
