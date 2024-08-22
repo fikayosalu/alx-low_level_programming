448060df9f45ade870ac33faa07e58498a962fc3
fikayosalu <salufikayo@gmail.com>
2024-08-22 13:10:23 +0100

    fixing

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -9,8 +9,14 @@
 int main(void)
 {
 	char *str;
+	char* s;
+	char *c;
 
 	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	s = "Holberton!";
+	c = "";
 	print_rev(str);
+	print_rev(s);
+	print_rev(c);
 	return (0);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -31,7 +31,7 @@ void print_rev(char *s)
 
 int _strlen(char *s)
 {
-	unsigned int i;
+	int i;
 
 	i = 0;
 	while (s[i] != '\0')
